8981adb83ff436e386d79f6d162c9bd4a1940b27
Macaque2077 <ubuntu@Theia>
2024-08-03 01:23:03 +1200

    better saving and logs

--- unpickle.py
+++
@@ -1,7 +0,0 @@
-file = '"C:\Users\youk3\Documents\A-codeprojects\classifier\data\cifar-10-python.tar.gz"'
-
-def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
