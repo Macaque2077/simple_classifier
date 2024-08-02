ada7fde8e3ffb61d3578393f000b1d377d4889cd
Macaque2077 <ubuntu@Theia>
2024-08-03 01:34:01 +1200

    better saving and logs

--- train.py
+++ train.py
@@ -2,27 +2,28 @@ from model import *
 from data_loaders import *
 import torch.nn as nn
 import torch
+import os
+import datetime
+from time import time 
 
 import numpy as np
-import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 
+import torch.optim as optim
 import logging #TODO use this
 
-logging.basicConfig(format="%(levelname)s:%(name)s:%(message)s" ,level=logging.INFO)
-ConsoleOutputHandler = logging.StreamHandler()
-logger = logging.getLogger('training')
-logger.addHandler(ConsoleOutputHandler)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('train')
 
 
-logger.info(torch.__version__)
-
-
-model_dir = './models/'
-
-
+logger.info(msg=torch.__version__)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-logger.info('Device: ', device)
+logger.info(msg='Device:  {device}')
 
+time_now = datetime.datetime.now().__format__('%H:%M:%S')
+model_dir = f'./models/{time_now}'
+os.makedirs(model_dir)
+writer = SummaryWriter(model_dir+'/logs')
 
 class trainer():
 	def __init__(self, net):
@@ -40,7 +41,7 @@ class trainer():
 		return(sum(p.numel() for p in self.model.parameters()))
 
 	def save_model(self, save_name:str):
-		logger.info(f'saving model at: {save_name}')
+		logger.info(msg=f'saving model at: {save_name}')
 		torch.save(self.model.state_dict(), save_name+'.wb')
 
 	def validate_epoch(self):
@@ -54,21 +55,22 @@ class trainer():
 				inferences = self.model(images)
 
 				loss = self.criterion(inferences, labels)
-
+				writer.add_scalar("Loss/validation", loss, self.epoch)
 				validation_loss += loss.item()
-				
-				validation_accuracy += torch.sum(torch.max(inferences.data, 1)[1] == labels)
+				accuracy = torch.sum(torch.max(inferences.data, 1)[1] == labels)
+				validation_accuracy += accuracy
+				writer.add_scalar("Accuracy/validation", accuracy, self.epoch)
 			
 			if self.lowest_validation_loss == None: self.lowest_validation_loss = validation_loss
 			if self.lowest_validation_accuracy ==None: self.lowest_validation_accuracy = validation_accuracy
 
 			if validation_loss < self.lowest_validation_loss:
-				logger.info('NEW LOWEST VAL LOSS')
+				logger.info(msg='NEW LOWEST VAL LOSS')
 				self.lowest_validation_loss = validation_loss
 				save_name = f'{model_dir}_{validation_loss}_{self.epoch}'
 				self.save_model(save_name)
 
-			logger.info(f'{self.epoch + 1}: Val_loss: {validation_loss / len(validationloader):.3f} accuracy: {100*(validation_accuracy // len(validationloader)):.3f}')
+			logger.info(msg=f'{self.epoch + 1}: Val_loss: {validation_loss / len(validationloader):.3f} accuracy%: {(validation_accuracy // len(validationloader)):.3f}')
 	
 	def run_epoch(self):
 		# loop over the dataset multiple times
@@ -92,13 +94,13 @@ class trainer():
 			train_loss += loss.item()
 			train_accuracy += torch.sum(torch.max(inferences.data, 1)[1] == labels)
 			if step % 100 == 99:    # print every 100 mini-batches
-				logger.info(f'{self.epoch + 1}-{step + 1:5d} train_loss: {train_loss / 100:.3f} train_accuracy: {train_accuracy / step:.3f}')
+				logger.info(msg=f'{self.epoch + 1}-{step + 1:5d} train_loss: {train_loss / 100:.3f} train_accuracy%: {train_accuracy / step:.3f}')
 				train_loss = 0.0
 
 		self.validate_epoch()
 	
 	def train_loop(self):
-		logger.info('Total params: ', self.calculate_params())
+		
 		for epoch in range(self.total_epochs): 
 			self.epoch = epoch
 			self.run_epoch()
@@ -116,15 +118,17 @@ class trainer():
 			loss = self.criterion(predicted, labels)
 			test_loss += loss.item()
 			test_accuracy += torch.sum(torch.max(inferences.data, 1) == labels)
-			logger.info(f'END RESULT test_loss: {test_loss / len(testloader):.3f} accuracy: {test_accuracy / len(testloader):.3f}')
+			logger.info(msg=f'END RESULT test_loss: {test_loss / len(testloader):.3f} accuracy: {test_accuracy / len(testloader):.3f}')
 
 if __name__ == '__main__':
-	logger.info('starting')
+	logger.info(msg='starting')
 	classifier = trainer(Net(256, 8))
 	total_parameters = classifier.calculate_params()
-	logger.info(f'Total number of parameters: {total_parameters}')
+	logger.info(msg=f'Total number of parameters: {total_parameters}')
 	classifier.train_loop()
 	classifier.test_model()
+	writer.flush()
 
 
-	logger.info('Finished Training')
+
+	logger.info(msg='Finished Training')
